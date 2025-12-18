0e94ba2f8adbc00c3c0b417d827e6f07a610c316
selvyputriayu <selvyputriayu32@gmail.com>
2025-12-18 11:11:57 +0700

    Create edukasi.php

---
+++ edukasi.php
@@ -0,0 +1,7 @@
+<h2>Edukasi Menjaga Lingkungan Sekolah</h2>
+<ul>
+    <li>Membuang sampah pada tempatnya</li>
+    <li>Melakukan piket kelas secara rutin</li>
+    <li>Menanam dan merawat tanaman</li>
+    <li>Menghemat air dan listrik</li>
+</ul>
